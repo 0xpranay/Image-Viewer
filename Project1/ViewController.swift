76bbe4008b2b91c5bdae760b2a0fedc61a32b572
Pranay Reddy <pranayreddy1024@gmail.com>
2020-07-07 12:01:02 +0530

    Started app template

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Pranay Reddy on 07/07/20.
+//  Copyright © 2020 Pranay_Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
